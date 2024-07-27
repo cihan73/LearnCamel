65b68d55b3c167e81d0a0d9c70a28cc7c414a690
Cihan <bulutyusufcihan@gmail.com>
2024-07-27 23:29:38 +0300

    card select and deselect

--- Assets/Scripts/RunTime/Card/Card.cs
+++ Assets/Scripts/RunTime/Card/Card.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
-   
+    public Transform context;
+    public void Prepare(/*CardData cardData*/)
+    {
+    //    ID = cardData.ID;
+    //    cardVisual.SetIcon(cardData.iconSprite);
+    //    Close();
+    }
+    public void Select()
+    {
+        transform.eulerAngles = new Vector3(0,0,0);
+    }
+    public void Deselect()
+    {
+        transform.eulerAngles = new Vector3 (0,180,0);
+    }
 }
